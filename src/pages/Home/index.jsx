7851522b9861c220891e5910dd8ec48a38182135
Chairuabda <chairuabda@gmail.com>
2023-10-10 23:53:30 +0700

    update Logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
 import { SideNavbar } from "../components/SideNavbar";
 import { UserBar } from "../components/UserBar";
 import { MidSection } from "../components/MidSection";
+import Logo from "../../assets/BlackAbstract.png";
 
 export const Home = () => {
 	return (
@@ -29,7 +30,10 @@ export const Home = () => {
 					left={"200"}
 					right={"200"}
 				>
-					<Box>Logo</Box>
+					<Box display={"flex"} alignItems={"center"}>
+						<Image src={Logo} h={"24px"} />
+						<Text ml={"5px"}>MySosmed</Text>
+					</Box>
 					<Box
 						border={"1px solid black"}
 						p={"2px 18px"}
